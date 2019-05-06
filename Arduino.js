f72effb284a562126e0ce2e5dbb1f1c25ad4085f
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-05-06 14:33:51 -0700

    Added more notes for work on arduino

--- Arduino.js
+++ Arduino.js
@@ -1,6 +1,6 @@
 SerialPort = require("serialport");
 Readline = require("@serialport/parser-readline");
-log = require("con-logger")
+log = require("con-logger");
 
 class Arduino {
     /**
@@ -16,17 +16,25 @@ class Arduino {
         this.deviceId = "";
         this.serialPort;
         this.parser;
-        this.status = 1;
+        this.status = 2; // 0 is good, 1 is error, 2 is unassigned
+        this.waterOnTimers = [];
+        this.waterOffTimers = [];
     }
+
+    // Method that sets the watering schedule whenever a new schedule comes in from the database
+    // Will clear previous timers, take in the object, parse it, and  set intervals based on the input
+    setWateringSchedule(schedule){ }
 
     // Method that returns the status of all connected devices and their data as an object
     getAllStatusAndData() { }
 
     // Method that gets the status and data of a specific connected device
     getStatusAndData(device) {
-        let data = this.getAllStatusAndData()
+        let data = this.getAllStatusAndData();
         switch (data) {
-            case pump:
+            case "pump":
+                break;
+            case "temp":
                 break;
             default:
                 break;
@@ -42,39 +50,42 @@ class Arduino {
     // Method that sends command to the arduino to turn the pump off
     stopWater() { }
 
+    // Method that sets the deviceId for the arduino based on this.deviceId
+    setDeviceId() {}
+
     // Method that takes in a response from the arduino and parses it appropriately
     handleResponse(response) { }
 
     // Method that initializes the serialport and parser. Must be called to initialize setup async
     setup() {
-        this.serialPort = new SerialPort(this.comName)
-        this.parser = this.serialPort.pipe(new Readline())
-        this.parser.on("data", this.handleResponse)
+        this.serialPort = new SerialPort(this.comName);
+        this.parser = this.serialPort.pipe(new Readline());
+        this.parser.on("data", this.handleResponse);
 
         return new Promise((resolve, reject) => {
-            let parser = this.serialPort.pipe(new Readline())
-            let ping = setInterval(() => this.serialPort.write("<status>"), 1000)
+            let parser = this.serialPort.pipe(new Readline());
+            let ping = setInterval(() => this.serialPort.write("<status>"), 1000);
 
             parser.on("data", data => {
-                let dataArr = data.split("~")
+                let dataArr = data.split("~");
                 if (dataArr[0] === "status") {
-                    this.status = parseInt(dataArr[1])
+                    this.status = parseInt(dataArr[1]);
                 } else {
-                    log(data)
+                    log(data);
                 }
-            })
+            });
 
             let check = setInterval(() => {
                 if (this.status > 0) {
                     clearInterval(check);
                     clearInterval(ping);
-                    resolve(0)
+                    resolve(0);
                 }
-            }, 500)
+            }, 500);
 
-            setTimeout(() => reject("timeout"), 60000)
+            setTimeout(() => reject("timeout"), 5000);
         })
     }
 }
 
-module.exports = Arduino
+module.exports = Arduino;
--- Pi.js
+++ Pi.js
@@ -1,30 +1,33 @@
 log = require("con-logger");
 Arduino = require("./Arduino");
+SerialPort = require("serialport");
 
 class Pi {
 
     constructor() {
         this.arduinos = [];
         this.status;
-        this.serialNumber;
+        this.deviceId;
         this.discovery = false; // This is set to true when it receives an update from the API
-        this.username;
-        this.password; // TODO: Make sure that this is hashed
-        // this.setup();
+        const username = "";
+        const password = "";
         // this.run();
     }
 
     // API call to the database to pull down any status updates. Should fire on ping from Pusher
-    getUpdate() { }
+    getUpdate() {
+
+    }
+
+    water() {}
 
     // Loop over all the arduinos and have them return their status with the serial number associated
     getAllStatus() {
         if (this.arduinos.length) {
             let data = {};
             this.arduinos.forEach(arduino => {
-                let stats = arduino.getAllStatus();
-                data[arduino.serialNumber] = stats;
-            })
+                data[arduino.serialNumber] = arduino.getAllStatus();
+            });
             return data;
         } else {
             return "No devices present"
@@ -34,54 +37,73 @@ class Pi {
     // Push the pi into a mode where it looks for a new arduino plugged in, checks to make sure it isn't already registered,
     //  then registers it to the database 
     async discover() {
-        // Allows discovery to run only when this.discover is true
-        while (this.discovery) {
             // Stores all ports that are available
             let ports = await SerialPort.list();
             ports.forEach(async port => {
                 // Checks to see if the port has a manufacturer and if that manufacturer includes arduino
                 if (port.manufacturer && port.manufacturer.split(" ").includes("Arduino")) {
                     // TODO: Add functionality to check and see if the id has been locally registered on a different port then change the port to match
-
                     // Checks to see if the arduino is already in the array of arduinos
                     if (!this.arduinos || !this.arduinos.some(arduino => arduino.serialNumber === port.serialNumber)) {
 
                         let newArd = new Arduino(port.comName, port.serialNumber);
-
                         // Setup is called seperately so that we can await properly. It will break after 60 seconds of inactivity and set discover to false
                         let setup = await newArd.setup();
                         if (setup === "timeout") {
                             this.discovery = false;
                             return;
                         }
-
                         // Set the deviceId of the new arduino
                         if (this.arduinos.length) {
                             newArd.deviceId = this.arduinos[this.arduinos.length - 1].deviceId + 1;
                         } else {
-                            newArd.deviceId = 0;
+                            newArd.deviceId = 1;
                         }
+                        newArd.setDeviceId();
                         // Add the arduinos to the local set of devices
                         this.arduinos.push(newArd);
-
                         // TODO: Push the new device up to the database and set discover to false both locally and on the api
                         log(newArd);
-                        return;
+                    }
+                }
+                // If the serial number  has been registered to a different comPort in the past, reassign it to the new one
+                // With the same schedule and id that it previously had
+                else if (this.arduinos && this.arduinos.some(arduino => (arduino.serialNumber === port.serialNumber && arduino.comName !== port.comName))) {
+                    for(let i = 0; i  < this.arduinos.length; i ++) {
+                        if(this.arduinos[i].serialNumber === port.serialNumber){
+                            let newArd = new Arduino(port.comName, port.serialNumber);
+                            // Setup is called seperately so that we can await properly. It will break after 60 seconds of inactivity and set discover to false
+                            let setup = await newArd.setup();
+                            if (setup === "timeout") {
+                                this.discovery = false;
+                                return;
+                            }
+                            newArd.deviceId = this.arduinos[i].deviceId;
+                            newArd.plantName  = this.arduinos[i].plantName;
+                            newArd.schedule = this.arduinos[i].schedule;
+                            newArd.status = 0;
+                            this.arduinos[i] = newArd;
+                        }
                     }
                 }
             })
-        }
     }
 
     // Primary method that will keep the program running and acting properly while it is on the pi
     // will respond to pusher to call this.getSchedule() and this.discover()
-    run() { }
+    run() {
+        this.setup();
+        while(true) {
+            // Pusher listener
+            this.discover();
+        }
+    }
 
     // Initial script that will have the user connect to wifi or plug in ethernet and put in their username/password
     // generated on the app. It will then log in and trigger the API to update to connected and set the status
-    // to 0 when the setup has completed correctly
+    // to 0 when the setup has completed correctly. **NOTE** Gets device id from account
     setup() { }
 
 }
 
-module.exports = Pi
+module.exports = Pi;
